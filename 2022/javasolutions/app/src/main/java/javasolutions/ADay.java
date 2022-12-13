a53a2fa37e7df59fa9420a977ff530379f5a67ba
Saahil Claypool <saahil@claypools.org>
2022-12-12 20:53:44 -0500

    WIP java project

---
+++ 2022/javasolutions/app/src/main/java/javasolutions/ADay.java
@@ -0,0 +1,11 @@
+package javasolutions;
+
+public abstract class ADay {
+    public String partA() throws Exception {
+        throw new Exception("A Implemented");
+    }
+
+    public String partB() throws Exception {
+        throw new Exception("A Implemented");
+    }
+}
---
+++ 2022/javasolutions/app/src/main/java/javasolutions/Day11.java
@@ -0,0 +1,11 @@
+package javasolutions;
+
+public class Day11 extends ADay {
+    public String partA() throws Exception {
+        throw new Exception("A Implemented");
+    }
+
+    public String partB() throws Exception {
+        throw new Exception("A Implemented");
+    }
+}
