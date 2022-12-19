1fe2702be9770a7ab9033de3877320ba23820bec
Saahil Claypool <saahil@claypools.org>
2022-12-19 16:15:40 -0500

    17 PartA

---
+++ 2022/csharp/Day17.cs
@@ -0,0 +1,132 @@
+namespace Aoc2022.Day17Solution;
+using Pos = Aoc2022.GridType.Pos;
+
+class World
+{
+    record State(long ShapeNumber, long WindNumber);
+    long ShapeNumber = 0;
+    long WindNumber = 0;
+    public List<int> Movements { get; set; } = new();
+    HashSet<Pos> Blocks { get; set; } = new();
+    public int Height => Blocks.Any() ? Blocks.Select(p => p.Row).Max() : 0;
+
+    public bool Occupied(Pos p) => Blocks.Contains(p) || p.Col < 0 || p.Col >= 7 || p.Row <= 0;
+    public bool Free(Pos p) => !Occupied(p);
+
+    List<State> States { get; set; } = new();
+
+    class Shape
+    {
+        public HashSet<Pos> InitialPoints { get; set; } = new();
+        public List<Pos> Move(int dx, int dy) => InitialPoints.Select(p => new Pos(p.Row + dy, p.Col + dx)).ToList();
+    }
+
+    public void DropShape()
+    {
+        var shape = Shapes[ShapeNumber++ % Shapes.Length];
+        var (X, Y) = (2, Height + 4);
+        var points = shape.Move(X, Y);
+        var turn = 0;
+        while (true)
+        {
+            turn++;
+            if (turn % 2 == 1) // start with wind
+            {
+                var dir = Movements[WindNumber++ % Movements.Count];
+                var newX = X + dir;
+                var newPoints = shape.Move(newX, Y);
+                // hit nothing -> valid move
+                if (newPoints.All(Free))
+                {
+                    points = newPoints;
+                    X = newX;
+                }
+
+            }
+            else
+            {
+                Y -= 1;
+                var newPoints = shape.Move(X, Y);
+                // hit something -> stop dropping
+                if (newPoints.Any(Occupied))
+                {
+                    break;
+                }
+                else
+                {
+                    points = newPoints;
+                }
+            }
+        }
+        Add(points);
+    }
+
+    void Add(IEnumerable<Pos> points)
+    {
+        foreach (var p in points)
+        {
+            Blocks.Add(p);
+        }
+    }
+
+    static readonly Shape[] Shapes = new[]
+    {
+        // line
+        new Shape() { InitialPoints = new() { new(0, 0), new(0, 1), new(0, 2), new(0, 3) } },
+        // cross
+        new Shape() { InitialPoints = new() { new(0, 1), new(1, 0), new(1, 1), new(1, 2), new(2, 1) } },
+        // L
+        new Shape() { InitialPoints = new() { new(2, 2), new(1, 2), new(0, 0), new(0, 1), new(0, 2) } },
+        // |
+        new Shape() { InitialPoints = new() { new(0, 0), new(1, 0), new(2, 0), new(3, 0) } },
+        // box
+        new Shape() { InitialPoints = new() { new(0, 0), new(0, 1), new(1, 1), new(1, 0) } }
+    };
+}
+
+public partial class Day17 : Day
+{
+    static World Parse(string input) => new() { Movements = input.Select(c => c == '<' ? -1 : 1).ToList() };
+    public override string SolveA(string input)
+    {
+        var world = Parse(input);
+
+        foreach (var _ in Enumerable.Range(0, 2022))
+        {
+            world.DropShape();
+        }
+
+        return world.Height.ToString();
+    }
+
+    public override string SolveB(string input)
+    {
+        var world = Parse(input);
+
+        for (var i = 0L; i < 1_000_000_000_000; i++)
+        {
+            world.DropShape();
+        }
+
+        return world.Height.ToString();
+    }
+
+    public Day17()
+    {
+        Tests = new()
+        {
+            new("A",
+            """
+            >>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>
+            """,
+            "3068",
+            SolveA),
+            new("B",
+            """
+            1
+            """,
+            "",
+            SolveB)
+        };
+    }
+}
