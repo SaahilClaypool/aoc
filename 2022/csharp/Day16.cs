b7146254686d500e53a8dbee6dd4d82f17d2e0ac
Saahil Claypool <saahil@claypools.org>
2022-12-19 14:32:25 -0500

    Day 16 part B

--- 2022/csharp/Day16.cs
+++ 2022/csharp/Day16.cs
@@ -9,7 +9,36 @@ public partial class Day16 : Day
     record OpenEvent(Valve Valve, int Minute);
     record State(List<OpenEvent> Events, int Minute)
     {
-        public int Score => Events.Sum(e => e.Minute * e.Valve.FlowRate);
+        int? cachedScore = null;
+        string? key = null;
+        public string Key
+        {
+            get
+            {
+                key ??= string.Join(".", Open.OrderBy(o => o));
+                return key;
+            }
+        }
+        public int Score
+        {
+            get
+            {
+                cachedScore ??= Events.Sum(e => e.Minute * e.Valve.FlowRate);
+                return cachedScore.Value;
+            }
+        }
+
+        HashSet<string>? _open = null!;
+
+        public HashSet<string> Open
+        {
+            get
+            {
+                _open ??= Events.Select(e => e.Valve.Name).ToHashSet();
+                return _open;
+            }
+        }
+
     }
 
 
@@ -18,18 +47,18 @@ public partial class Day16 : Day
         var tunnels = Parse(input);
         var distances = DistanceToHighFlow(tunnels);
 
-        var pq = new PriorityQueue<State, int>();
-        pq.Enqueue(new(new(), 30), 0);
+        var pq = new Queue<State>();
+        pq.Enqueue(new(new(), 30));
 
         var allOrders = new List<State>();
         var best = pq.Peek();
-        while (pq.TryDequeue(out var curState, out var score))
+        while (pq.TryDequeue(out var curState))
         {
             if (curState.Minute <= 0)
             {
                 continue;
             }
-            if (best.Score < score)
+            if (best.Score < curState.Score)
             {
                 best = curState;
             }
@@ -52,7 +81,7 @@ public partial class Day16 : Day
                         .Append(new(tunnels[nextOpen], curState.Minute - distance - 1))
                         .ToList(),
                     curState.Minute - distance - 1);
-                pq.Enqueue(nextState, nextState.Score);
+                pq.Enqueue(nextState);
             }
         }
 
@@ -63,7 +92,75 @@ public partial class Day16 : Day
 
     public override string SolveB(string input)
     {
-        throw new NotImplementedException();
+        var tunnels = Parse(input);
+        var distances = DistanceToHighFlow(tunnels);
+
+        var pq = new Queue<State>();
+        pq.Enqueue(new(new(), 26));
+
+        var allOrders = new List<State>();
+        var best = pq.Peek();
+        while (pq.TryDequeue(out var curState))
+        {
+            allOrders.Add(curState);
+            if (curState.Minute <= 0)
+            {
+                continue;
+            }
+            if (best.Score < curState.Score)
+            {
+                best = curState;
+            }
+            var currentSpot = curState.Events.LastOrDefault()?.Valve?.Name ?? "AA";
+            var curOpen = curState.Events.Select(e => e.Valve.Name).ToHashSet();
+            foreach (var (nextOpen, distance) in distances[currentSpot])
+            {
+                if (curOpen.Count == distances["AA"].Count)
+                {
+                    continue;
+                }
+                if (curOpen.Contains(nextOpen))
+                {
+                    continue;
+                }
+                var nextState = new State(
+                    curState
+                        .Events
+                        .Append(new(tunnels[nextOpen], curState.Minute - distance - 1))
+                        .ToList(),
+                    curState.Minute - distance - 1);
+                pq.Enqueue(nextState);
+            }
+        }
+
+        allOrders = allOrders.OrderByDescending(o => o.Score).DistinctBy(o => o.Key).ToList();
+
+        Console.WriteLine($"Comparing {allOrders.Count * allOrders.Count}");
+
+        var bestPairScore = 0;
+        for (var i = 0; i < allOrders.Count; i++)
+        {
+            var order = allOrders[i];
+            for (var j = 0; j < i; j++)
+            {
+                var elephantOrder = allOrders[j];
+                if (elephantOrder.Open.Any(order.Open.Contains))
+                {
+                    continue;
+                }
+                var pair = (order, elephantOrder with
+                {
+                    Events = elephantOrder.Events.ToList(),
+                });
+                var pairScore = pair.order.Score + pair.Item2.Score;
+                if (pairScore > bestPairScore)
+                {
+                    bestPairScore = pairScore;
+                }
+            }
+        }
+
+        return bestPairScore.ToString();
     }
 
     public Day16()
@@ -87,10 +184,19 @@ public partial class Day16 : Day
             SolveA),
             new("B",
             """
-            1
+            Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+            Valve BB has flow rate=13; tunnels lead to valves CC, AA
+            Valve CC has flow rate=2; tunnels lead to valves DD, BB
+            Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+            Valve EE has flow rate=3; tunnels lead to valves FF, DD
+            Valve FF has flow rate=0; tunnels lead to valves EE, GG
+            Valve GG has flow rate=0; tunnels lead to valves FF, HH
+            Valve HH has flow rate=22; tunnel leads to valve GG
+            Valve II has flow rate=0; tunnels lead to valves AA, JJ
+            Valve JJ has flow rate=21; tunnel leads to valve II
             """,
-            "",
-            SolveB)
+            "1707",
+            SolveB),
         };
     }
 
@@ -153,7 +259,7 @@ public partial class Day16 : Day
         }
 
         return dict
-            .Select(v => 
+            .Select(v =>
                 (v.Key, Value: v.Value.Where(v => targetTunnels.ContainsKey(v.Key)).ToDictionary(v => v.Key, v => v.Value)))
             .ToDictionary(v => v.Key, v => v.Value);
     }
