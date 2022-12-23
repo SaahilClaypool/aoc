a98a2174a7be00d7990dbfed0312da3120253c6e
Saahil Claypool <saahil@claypools.org>
2022-12-23 10:40:58 -0500

    22 part 2 WIP

--- 2022/csharp/Day22.cs
+++ 2022/csharp/Day22.cs
@@ -4,13 +4,14 @@ using System.Text;
 using static Aoc.Runner.Helpers;
 using static Aoc2022.Facing;
 
-enum Facing { R = 0, D = 1, L = 2, U = 3};
+enum Facing { R = 0, D = 1, L = 2, U = 3 };
 
 public partial class Day22 : Day
 {
     record Pos(int R, int C);
     class Grid
     {
+        public Dictionary<int, (int RowMin, int RowMax, int ColMin, int ColMax)> SideRanges { get; set; } = new();
         readonly DefaultDictionary<int, (int, int)> colRanges = new(() => (int.MaxValue, int.MinValue));
         readonly DefaultDictionary<int, (int, int)> rowRanges = new(() => (int.MaxValue, int.MinValue));
         public Grid(DefaultDictionary<Pos, char> data)
@@ -56,9 +57,9 @@ public partial class Day22 : Day
 
             static int wrap(int a, int min, int max) =>
                 (a < min) ?
-                    max:
+                    max :
                 (a > max) ?
-                    min:
+                    min :
                     a;
 
             if (f == R || f == L)
@@ -67,6 +68,64 @@ public partial class Day22 : Day
                 return new(wrap(p.R, rowMin, rowMax), p.C);
 
         }
+
+        public (Pos, Facing) WrapCube(Pos p, int side, Facing facing)
+        {
+            var (rmin, rmax, cmin, cmax) = SideRanges[side];
+            int newSide;
+            Facing newFacing;
+            Pos newPos;
+            if (p.R > rmax) // down
+                (newSide, newFacing, newPos) = side switch
+                {
+                    1 => (4, D, p),
+                    2 => (5, U),
+                    3 => (5 , R),
+                    4 => (5, D), 
+                    5 => (2, U),
+                    6 => (2, R),
+                    _ => throw new Exception()
+                };
+            else if (p.R < rmin) // up
+                (newSide, newFacing) = side switch
+                {
+                    1 => (2, D),
+                    2 => (1, D),
+                    3 => (1 , R),
+                    4 => (1, U), 
+                    5 => (4, U),
+                    6 => (4, L),
+                    _ => throw new Exception()
+                };
+            else if (p.C < cmin) // left
+                (newSide, newFacing) = side switch
+                {
+                    1 => (3, D),
+                    2 => (6, U),
+                    3 => (2 , L),
+                    4 => (3, L), 
+                    5 => (3, U),
+                    6 => (5, L),
+                    _ => throw new Exception()
+                };
+            else if (p.C > cmax) // right
+                (newSide, newFacing) = side switch
+                {
+                    1 => (6, L),
+                    2 => (3, R),
+                    3 => (4 , R),
+                    4 => (6, D), 
+                    5 => (6, R),
+                    6 => (1, L),
+                    _ => throw new Exception()
+                };
+        }
+
+        public int Side(Pos p) => SideRanges.WithIndex().First(r =>
+        {
+            var (rmin, rmax, cmin, cmax) = r.Item1.Value;
+            return p.R <= rmax && p.R >= rmin && p.C <= cmax && p.C >= cmin;
+        }).Item2;
     }
 
     record Command(Facing Turn, int Count);
@@ -80,7 +139,7 @@ public partial class Day22 : Day
         var pos = grid.Wrap(new(1, int.MaxValue), L);
         foreach (var command in commands)
         {
-            facing = 
+            facing =
                 facing switch
                 {
                     R => command.Turn == L ? U : D,
