79ad42b476f01294ddc2df7a0de3dd0efbb1243d
Saahil Claypool <saahil@claypools.org>
2023-12-11 22:47:20 -0500

    part a done

---
+++ 2023/fsharp/Day11.fs
@@ -0,0 +1,80 @@
+module Aoc.Solutions.Y2023.Day11
+
+open Aoc.Runner
+open System
+open System.Text.RegularExpressions
+open System.Collections.Generic
+
+let parse (input: string) =
+    input
+    |> lines
+    |> Seq.indexed
+    |> Seq.collect (fun (row, line) ->
+        line
+        |> Seq.indexed
+        |> Seq.choose (fun (col, c) ->
+        match c with 
+        | '#' -> Some (int64 row, int64 col)
+        | _ -> None
+        )
+    )
+
+let distance (a: (int64 * int64)) (b: (int64 * int64)) =
+    Math.Abs((fst a) - (fst b)) + Math.Abs((snd a) - (snd b))
+
+
+let expandGalaxy (factor: int64) (galaxyRows: Set<int64>) (galaxyCols: Set<int64>) (galaxy: (int64 * int64)) =
+    let (row, col) = galaxy
+    let occupiedRows = galaxyRows |> Seq.filter (fun r -> r < row) |> Seq.length |> int64
+    let occupiedCols = galaxyCols |> Seq.filter (fun c -> c < col) |> Seq.length |> int64
+
+
+    let expanded = (row * 2L - occupiedRows, col * 2L - occupiedCols)
+    expanded
+
+let allPairCombos a =
+    seq {
+        for i in 0..(Seq.length a - 1) do
+            for j in (i + 1)..(Seq.length a - 1) do
+                a |> Seq.item i, a |> Seq.item j
+    }
+
+let sample = raw"""
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+"""
+
+type Day11() =
+    inherit Day()
+
+    override _.SolveA input =
+        let galaxies = parse input
+        let occupiedRows = galaxies |> Seq.map fst |> Set.ofSeq
+        let occupiedCols = galaxies |> Seq.map snd |> Set.ofSeq
+        let expanded = galaxies |> Seq.map (expandGalaxy 2L occupiedRows occupiedCols) |> Seq.toList
+        let pairs = expanded |> allPairCombos |> List.ofSeq
+        let totalDistance = 
+            pairs
+            |> Seq.map (fun (left, right) ->
+                let d = distance left right
+                d)
+            |> Seq.sum
+
+        totalDistance |> string
+
+    override _.SolveB input =
+        ""
+    override this.Tests =
+        [
+            Test("a", sample, "374", fun x -> this.SolveA x);
+        ]
+        |> List<Test>
+    
